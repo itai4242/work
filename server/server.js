a15b182e90e0b5cbc2729fb8cc4f625da51a8a0e
itai4242 <itai4230@gmail.com>
2019-02-19 16:49:52 +0200

    end

--- server/server.js
+++ server/server.js
@@ -14,61 +14,94 @@ const port = process.env.PORT;
 const publicPath = path.join(__dirname, '../views')
 app.use(express.static(publicPath));
 
-// app.use(function (req, res, next) {
-//     res.renderWithData = function (view, model, data) {
-//         model.data = JSON.stringify(data);
-//         res.render(view, model);
-//     };
-//     next();
-// });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.set('view engine', 'ejs');
+
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-// app.set('view cache', true);
+
 
 app.post('/addMovie.html',(req,res) => {
+
   var movie = new Movie ({
     name:req.body.name,
     genre:req.body.genre,
     year:parseInt(req.body.year)
   });
-  var message;
   Movie.findOne({name:movie.name}).then((movieExist) =>{
     if(movieExist) {
-      //return callback('this movie is currently on the collection')
-      message='this movie is currently on the collection';
-      // return res.render('addMovie.html',{message:'this movie is currently on the collection'})
+
+      res.send('this movie is currently on the collection');
+
     }else{
       if (movie.year<1900||movie.year>2020){
-        // return callback('wrong year. please try again between 1900-2020')
-        message='wrong year. please try again between 1900-2020';
-        // return res.render('addMovie.html',{message:'wrong year. please try again between 1900-2020'})
+
+        res.send('wrong year. please try again between 1900-2020');
+
       }else{
         movie.save().then((doc) => {
-          // io.emit('updateMovieList', undefined);
-          message='movie added to the database';
-          // res.render('addMovie.html',{message:'movie added to the database'})
+
+          res.send('movie added to the database');
+
         }, (e) => {
-          // io.emit('errorone', e);
-          message='something went worng';
-          // res.render('addMovie.html',{message:'something went worng'})
+
+          res.send('something went worng');
+
         });
         }
       }
-        // res.render('addMovie.html');
-      //   var someData= {message};
-      //   const Http = new XMLHttpRequest();
-      //   const url='http://localhost:3000/addMovie.html';
-      //   Http.open("GET", url);
-      //   Http.send({ data: JSON.stringify(someData) });
-      //   Http.onreadystatechange=(e)=>{
-      //   console.log(Http.responseText)
-      // }
+  })
+})
+app.patch('/addRating.html', (req,res) => {
 
+   Movie.findOne({name:req.body.name}).then((movieExist) =>{
+    if(movieExist) {
+      var votes = movieExist.votes+1;
+      if(req.body.rating===''){
+        res.send('you have to add rating')
+      }
+      var rating =parseFloat(req.body.rating)
+      if (rating>10 || rating <0){
+        res.send('invalid rating please enter rating between 0-10')
+      }
+      rating = (rating+movieExist.rating*movieExist.votes)/votes
+      var body ={rating,votes}
+      Movie.findOneAndUpdate({name:req.body.name},{$set: body}, {new: true}).then(movieExist => {
+
+        res.send(''+movieExist.rating)
+      })
+    } else {
+      res.send('no movie in this name on the database')
+    }
+  }).catch((e)=>{
+    res.send('something went worng');
+  })
+})
+app.post('/search.html', (req,res)=>{
+
+  var yearStart = parseInt(req.body.yearStart)
+  var yearEnd = parseInt(req.body.yearEnd)
+  if(yearStart<1900||yearStart>2020||yearEnd<1900||yearEnd>2020){
+
+    return res.status(201).send('the years for search must be between 1900-2020')
+  }
+  Movie.find({
+    genre:req.body.genre,
+    year:{ $gte: yearStart, $lte: yearEnd }
+  }).then((movies)=>{
+    if(movies.length>0){
+
+      res.status(200).send(movies)
+    }else {
+
+      res.status(201).send('didnt find movies');
+    }
+
+  }).catch((e)=>{
+
+    res.status(201).send('something went worng')
   })
 })
 
